c428d3712fe543ac9f6b0264007ee53011058c4e
Naman Tiwari <www.skynetc52@gmail.com>
2025-10-28 13:29:08 +0530

    added env variable correctly

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@ import helmet from 'helmet';
 
 dotenv.config();
 const app = express();
-const port = process.env.port;
+const port = process.env.PORT;
 app.use(cors({
     origin: [
         process.env.FRONTEND_OLD_URL,   // production old frontend
